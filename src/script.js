2d7da488109af9556bc49f981842d1df00dfa6b7
Florian <florianmtddev@gmail.com>
2025-09-29 13:54:11 +0200

    Color tweaks

--- src/script.js
+++ src/script.js
@@ -9,6 +9,7 @@ import waterFragmentShader from './shaders/water/fragment.glsl'
  */
 // Debug
 const gui = new GUI({ width: 340 })
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -22,6 +23,10 @@ const scene = new THREE.Scene()
 // Geometry
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
+// Colors
+debugObject.depthColor = '#0000ff'
+debugObject.surfaceColor = '#8888ff'
+
 // Material
 const waterMaterial = new THREE.ShaderMaterial(
     {
@@ -32,10 +37,18 @@ const waterMaterial = new THREE.ShaderMaterial(
             uTime: { value: 0 },
             uBigWavesElevation: { value: 0.2 },
             uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
-            uBigWavesSpeed: { value: 0.75 }
+            uBigWavesSpeed: { value: 0.75 },
+            uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+            uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+            uColorOffset: { value: 0.25 },
+            uColorMultiplier: { value: 2 },
         }
 })
 
+gui.addColor(debugObject, 'depthColor').onChange(() => {
+    waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor) })
+gui.addColor(debugObject, 'surfaceColor').onChange(() => {
+    waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor) })
 gui.add(waterMaterial.uniforms.uBigWavesElevation,
     'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value,
@@ -43,6 +56,8 @@ gui.add(waterMaterial.uniforms.uBigWavesFrequency.value,
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value,
     'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
 gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
+gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset')
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -3,16 +3,24 @@ uniform vec2 uBigWavesFrequency;
 uniform float uTime;
 uniform float uBigWavesSpeed;
 
+varying float vElevation;
+
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     // Elevation
-    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) * uBigWavesElevation;
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * 
+    sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) 
+    * uBigWavesElevation;
+
     modelPosition.y += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    // Varyings
+    vElevation = elevation;
 
     gl_Position = projectedPosition;
 }
